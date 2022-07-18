3654723b913ffd78df90c57d8e23b1c2430fb812
jagadeesh9948 <jagadeeshsairowthula@gmail.com>
2022-07-18 12:05:14 +0530

    commands

---
+++ index.js
@@ -0,0 +1,49 @@
+const express=require ("express");
+const app=express();
+const PORT=process.env.PORT||4000;
+const mysql=require ("mysql")
+
+
+
+const connection=mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"jagadeesh9948",
+    database:"ddl_commands"
+})
+
+connection.connect(e=>{
+    if(e){
+        console.log(e)
+        return e
+    }
+    else console.log("connected successfully")
+})
+
+const users={
+    name:"jagadeesh",
+    age:23,
+    address:"vijayawada",
+    email:"jagadeeshsairowthula@gmail.com"
+}
+
+app.get("/",(req,res)=>{
+    res.send("hello this is default root route")
+})
+app.get("/users",(req,res)=>{
+    res.json(users)
+})
+app.get("/getusers",(req,res)=>{
+    connection.query("select * from users",(err,result)=>{
+         if(err)
+            return res.send(err)
+         else
+            return res.json(result)
+
+    })
+})
+
+
+
+
+app.listen(PORT,()=>console.log(`server is running on port ${PORT}`))
